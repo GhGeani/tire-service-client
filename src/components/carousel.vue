b23bba4d6d7bef104aad2818d66ae4954bb259fd
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-16 01:37:40 +0300

    carousel component

---
+++ src/components/carousel.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .carousel.slide#carouselExampleIndicators(data-ride='carousel')
+    ol.carousel-indicators
+      li.active(data-target='#carouselExampleIndicators' data-slide-to='0')
+      li(data-target='#carouselExampleIndicators' data-slide-to='1')
+      li(data-target='#carouselExampleIndicators' data-slide-to='2')
+    .carousel-inner
+      .carousel-item.active
+        img.d-block.w-100(src='../assets/logo.png' alt='First Slide')
+      .carousel-item
+        img.d-block.w-100(src='../assets/logo.png' alt='Second Slide')
+      .carousel-item
+        img.d-block.w-100(src='../assets/logo.png' alt='Third Slide')
+    a.carousel-control-prev(href="#carouselExampleIndicators" role="button" data-slide="prev")
+      span.carousel-control-prev-icon(aria-hidden="true")
+      span.sr-only Previous
+    a.carousel-control-next(href="#carouselExampleIndicators" role="button" data-slide="next")
+      span.carousel-control-next-icon(aria-hidden="true")
+      span.sr-only Next
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+.carousel-item
+  height 300px
+  background-color lightgrey
+  opacity 0.8
+</style>
