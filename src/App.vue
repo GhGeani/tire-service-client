a1626b89c70b8be890fb5e02dd2ec857fcb9175d
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-16 01:37:16 +0300

    init app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+main
+  navi
+  carousel
+  .container
+    router-view
+</template>
+
+<script>
+import navi from './components/navi'
+import carousel from './components/carousel'
+
+export default {
+  name: 'tire-service',
+  components: {
+    navi,
+    carousel
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/fontawesome.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/brands.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/solid.css';
+import 'axios';
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app')
