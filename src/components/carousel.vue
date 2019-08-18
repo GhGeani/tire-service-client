3c8699209ccfebcde80939b2010ba357ba86b3c3
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-19 00:37:24 +0300

    Add .container class

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
-  .carousel.slide#carouselExampleIndicators(data-ride='carousel')
-    ol.carousel-indicators
-      li.active(data-target='#carouselExampleIndicators' data-slide-to='0')
-      li(data-target='#carouselExampleIndicators' data-slide-to='1')
-      li(data-target='#carouselExampleIndicators' data-slide-to='2')
-    .carousel-inner
-      .carousel-item.active
-        img.d-block.w-100(src='../assets/img1.jpeg' alt='First Slide')
-      .carousel-item
-        img.d-block.w-100(src='../assets/img2.jpeg' alt='Second Slide')
-    a.carousel-control-prev(href="#carouselExampleIndicators" role="button" data-slide="prev")
-      span.carousel-control-prev-icon(aria-hidden="true")
-      span.sr-only Previous
-    a.carousel-control-next(href="#carouselExampleIndicators" role="button" data-slide="next")
-      span.carousel-control-next-icon(aria-hidden="true")
-      span.sr-only Next
+    .container.carousel.slide#carouselExampleIndicators(data-ride='carousel')
+      ol.carousel-indicators
+        li.active(data-target='#carouselExampleIndicators' data-slide-to='0')
+        li(data-target='#carouselExampleIndicators' data-slide-to='1')
+        li(data-target='#carouselExampleIndicators' data-slide-to='2')
+      .carousel-inner
+        .carousel-item.active
+          img.d-block.w-100(src='../assets/img1.jpeg' alt='First Slide')
+        .carousel-item
+          img.d-block.w-100(src='../assets/img2.jpeg' alt='Second Slide')
+      a.carousel-control-prev(href="#carouselExampleIndicators" role="button" data-slide="prev")
+        span.carousel-control-prev-icon(aria-hidden="true")
+        span.sr-only Previous
+      a.carousel-control-next(href="#carouselExampleIndicators" role="button" data-slide="next")
+        span.carousel-control-next-icon(aria-hidden="true")
+        span.sr-only Next
 </template>
 
 <script>
