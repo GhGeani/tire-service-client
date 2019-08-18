b0e564a82481732d66854a2c14f2a6e88ca831b5
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-19 00:43:03 +0300

    Schema for service resoarce

---
+++ src/views/services/list.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  section.border-bottom.border-warning
+    .container(style="background-color: #EA846F")
+      .text-white.text-center.title Serviciile oferite
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus">
+.title
+  font-size 30px
+</style>
---
+++ src/views/services/service.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+
+</template>
+
+<script>
+export default {
+  deta(){
+    return {
+      title: '',
+      description: ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
