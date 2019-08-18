c98362ca9ba394a417fc766d95efeed275f7135b
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-19 00:41:49 +0300

    Shema for sales resource

---
+++ src/views/sales/list.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  section.border-bottom.border-warning
+    .container(style="background-color: #EA846F")
+      .text-white.text-center.title Vânzări
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
