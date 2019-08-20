25976129149bb5f5ddfe8bd48207f0bd4eee9c3a
GhGeani <gherghe_geani@yahoo.com>
2019-08-20 09:30:13 +0300

    fix with bind

--- src/views/services/service.vue
+++ src/views/services/service.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .list-group-item.list-group-item(id='list-' + ${name + '-list' data-toggle="list" href="#list-" + name role="tab" aria-controls=name) {{ name }}
+  a.list-group-item.list-group-item(id='list-' + name + '-list' data-toggle="list" v-bind:href="`#list-${name}`" role="tab" aria-controls=name) {{ name }}
 </template>
 
 <script>
@@ -7,7 +7,7 @@ export default {
   props: {
     name: String,
     description: String
-  }
+  },
 }
 </script>
 
