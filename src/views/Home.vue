358e863322590779a92edd7e97d09b97cbf24ca1
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-26 00:53:21 +0300

    remove announce list from router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template lang="pug">
 
-  .row.justify-content-center
-    .col-12.col-sm-12.col-md-12.col-lg-4.col-xl-4
-      announceList.border-bottom.border-warning
-    .col-12.col-sm-12.col-md-12.col-lg-8.col-xl-8
+  .row.mt-5.pt-5
+    .col-12
       router-view
 </template>
 
 <script>
-import announceList from './announce/list';
 import saleList from './sales/list';
 import serviceList from './services/list';
 
 export default {
   components: {
-    announceList,
     saleList,
     serviceList,
   }
