50ef43c053942668a4c9683cf3424d0ab70d0f36
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-26 00:53:58 +0300

    init sales view

---
+++ src/views/sales/main.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  section.contianer
+    .row
+      .container(style="background-color: #EA846F")
+        .text-white.text-center.title Vânzări
+      list(v-if="response.status === 200" :response="response.data")
+      h5.m-auto.text-muted(v-else) {{ response.statusText }}
+</template>
+
+<script>
+import list from './list'
+
+export default {
+  components: {
+    list
+  },
+  data() {
+    return {
+      response: {}
+    }
+  },
+  async mounted() {
+    await this.getAll();
+    console.log(this.response);
+  },
+  methods: {
+    async getAll() {
+      this.response = await this.$http.get('http://localhost:3000/items?page=1') 
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/views/sales/sale.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  section.col-12.col-md-12.col-lg-4
+    .card
+      img.card-img-top(v-bind:src="`http://localhost:3000/files/${images[0]}`")
+        
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    description: String,
+    images: Array,
+    date: String
+  },
+  mounted() {
+    console.log(this.images[0]);
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
