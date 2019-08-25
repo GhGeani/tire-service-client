4df6f503644c97023b303db617b96b0e01fc55fc
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-26 00:51:27 +0300

    carousel id change

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,18 +1,18 @@
 <template lang="pug">
-    .container.carousel.slide#carouselExampleIndicators(data-ride='carousel')
+    .carousel.mh-100.slide#imgs(data-ride='carousel')
       ol.carousel-indicators
-        li.active(data-target='#carouselExampleIndicators' data-slide-to='0')
-        li(data-target='#carouselExampleIndicators' data-slide-to='1')
-        li(data-target='#carouselExampleIndicators' data-slide-to='2')
+        li.active(data-target='#imgs' data-slide-to='0')
+        li(data-target='#imgs' data-slide-to='1')
+        li(data-target='#imgs' data-slide-to='2')
       .carousel-inner
         .carousel-item.active
-          img.d-block.w-100(src='../assets/img1.jpeg' alt='First Slide')
+          img.d-block.w-100.h-100(src='../assets/img1.jpeg' alt='First Slide')
         .carousel-item
-          img.d-block.w-100(src='../assets/img2.jpeg' alt='Second Slide')
-      a.carousel-control-prev(href="#carouselExampleIndicators" role="button" data-slide="prev")
+          img.d-block.w-100.h-100(src='../assets/img2.jpeg' alt='Second Slide')
+      a.carousel-control-prev(href="#imgs" role="button" data-slide="prev")
         span.carousel-control-prev-icon(aria-hidden="true")
         span.sr-only Previous
-      a.carousel-control-next(href="#carouselExampleIndicators" role="button" data-slide="next")
+      a.carousel-control-next(href="#imgs" role="button" data-slide="next")
         span.carousel-control-next-icon(aria-hidden="true")
         span.sr-only Next
 </template>
