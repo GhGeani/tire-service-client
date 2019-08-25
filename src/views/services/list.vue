7858bd0554272a05585b0c638e82d7a945136973
Gherghe Geani <gherghe_geani@yahoo.com>
2019-08-26 00:53:43 +0300

    small changes

--- src/views/services/list.vue
+++ src/views/services/list.vue
@@ -1,16 +1,14 @@
 <template lang="pug">
-  section.border-bottom.border-warning
-    .container(style="background-color: #EA846F")
-      .text-white.text-center.title Serviciile oferite
-    .list-group#list-tab(v-if="response.status === 200" role="tablist")
+  section
+    .container.add-border-bottom
+      h3.text-muted.text-center.title Serviciile oferite
+    .row(v-if="response.status === 200")
       service(v-for="service in response.data" :name="service.name" :description="service.description")
     .h5.text-muted.text-center(v-else) {{ response.statusText }}
 </template>
 
 <script>
 import service from './service';
-
-import axios from 'axios';
 
 export default {
   data() {
@@ -23,17 +21,15 @@ export default {
   },
   methods: {
     async getAll() {
-      this.response = await axios.get('http://localhost:3000/infos');
+      this.response = await this.$http.get('http://localhost:3000/infos');
     }
   },
   async mounted() {
     await this.getAll();
-    console.log(this.response);
   }
 }
 </script>
 
-<style lang="stylus">
-.title
-  font-size 30px
+<style lang="stylus" scoped>
+
 </style>
