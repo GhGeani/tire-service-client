e8f9cd0861c1b8aaaa2e16494b8d0926d955aa99
Gherghe Geani <gherghe_geani@yahoo.com>
2019-09-02 23:39:18 +0300

    add contact and some classes

--- src/views/services/list.vue
+++ src/views/services/list.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
-  section
-    .container.add-border-bottom
+  section.container
+    .container.add-border-bottom.mt-5
       h3.text-muted.text-center.title Serviciile oferite
     .row(v-if="response.status === 200")
       service(v-for="service in response.data" :name="service.name" :description="service.description")
     .h5.text-muted.text-center(v-else) {{ response.statusText }}
+    footer.container.mt-4
+      contact
 </template>
 
 <script>
 import service from './service';
+
+import contact from '../../components/contact'
 
 export default {
   data() {
@@ -17,7 +21,8 @@ export default {
     }
   },
   components: {
-    service
+    service,
+    contact
   },
   methods: {
     async getAll() {
