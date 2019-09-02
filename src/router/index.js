53801204b6f5d3331900192deb5fe45587d815b9
Gherghe Geani <gherghe_geani@yahoo.com>
2019-09-02 23:34:21 +0300

    create route for contact and get sale

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Services from '../views/services/list.vue';
+import Home from '../views/Home.vue';
 import Sales from '../views/sales/main.vue';
+import Sale from '../views/sales/getSale.vue';
+import Contact from '../components/contact.vue';
+
 
 Vue.use(Router);
 
@@ -11,7 +15,7 @@ export default new Router ({
     {
       path: '/',
       name: 'Home',
-      component: Services
+      component: Home
     },
     {
       path: '/servicii',
@@ -22,6 +26,16 @@ export default new Router ({
       path: '/vanzari',
       name: 'Sales',
       component: Sales
-    }
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/vanzari/:id',
+      name: 'Sale',
+      component: Sale
+    },
   ]
 });
