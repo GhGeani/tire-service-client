2da8b16d89f075d71a2b4fd5e1504196fc77d890
Gherghe Geani <gherghe_geani@yahoo.com>
2019-09-02 23:35:20 +0300

    remove uness classes

--- src/views/announce/announce.vue
+++ src/views/announce/announce.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  li.list-group-item.list-group-item-action.flex-column.align-items-start
+  li.list-group-item.list-group-item-action
     small.float-right {{ date }}
     p.mb-1.mt-4 {{ description }}
 </template>
