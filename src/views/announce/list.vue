8dd98095f2b61a5e15ae85e0824683e554d982c2
Gherghe Geani <gherghe_geani@yahoo.com>
2019-09-02 23:35:55 +0300

    class changes

--- src/views/announce/list.vue
+++ src/views/announce/list.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-  section.container
+  section
     .row
       .container.add-border-bottom.text-center.text-muted
         span.title Anunțuri
     .row.mt-4(v-if="response.status === 200")
-      ul.list-group
+      .container
         announce(v-for="announce in response.data.result" :description="announce.description" :date="announce.date")
     .h5.text-muted.text-center(v-else) {{ response.statusText }}
 </template>
